817bc8ecc6d031a3d7e4b03911fd81f58814d349
Ailamar Alves <ailamar.alvesg@gmail.com>
2021-02-20 16:07:34 -0300

    Add message after search and new cities

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ function pesquisar() {
     var loc = document.getElementById('locinicial')
     var dest = document.getElementById('destino')
 
+    if (dest.value == loc.value) {
+        document.getElementById("destino").onclick = function() {
+            dest.remove(i);
+        };
+    }
+
     if (loc.value.length == '' || dest.value == '') {
-        window.alert('[ERROR] Verifique os dados e tente novamente!')
+        window.alert('[ERROR] Não é possível pesquisar campos vazios. Verifique os dados e tente novamente!')
+    } else {
+        document.getElementById('rotaPesquisada').innerHTML = 'As rota selecionada passa pelas seguintes cidades: ';
     }
 
 }
